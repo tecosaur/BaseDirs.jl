fec68205ad6d66545f6298c7a4ca5b0ec3bc1c13
TEC <git@tecosaur.net>
2023-01-26 19:24:24 +0800

    Move variable definitions to own file

--- src/XDG.jl
+++ src/XDG.jl
@@ -1,31 +1,6 @@
 module XDG
 
-# Base directory
-
-const DATA_HOME = Ref("")
-const DATA_DIRS = Ref([""])
-const CONFIG_HOME = Ref("")
-const CONFIG_DIRS = Ref([""])
-const BIN_HOME = Ref("") # Not yet standardized.
-const STATE_HOME = Ref("")
-const CACHE_HOME = Ref("")
-const RUNTIME_DIR = Ref("")
-
-# User directories
-
-const DESKTOP_DIR = Ref("")
-const DOWNLOAD_DIR = Ref("")
-const DOCUMENTS_DIR = Ref("")
-const MUSIC_DIR = Ref("")
-const PICTURES_DIR = Ref("")
-const VIDEOS_DIR = Ref("")
-const TEMPLATES_DIR = Ref("")
-const PUBLICSHARE_DIR = Ref("")
-
-# Other directories
-
-const APPLICATIONS_DIRS = Ref([""])
-const FONTS_DIRS = Ref([""])
+include("variables.jl")
 
 function reload end
 function projectpath end
