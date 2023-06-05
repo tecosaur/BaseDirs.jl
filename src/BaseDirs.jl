1ba65ef7db05c22ec34f48d756f9df2d7fca0227
TEC <git@tecosaur.net>
2023-06-05 20:29:30 +0800

    Precompile everything

--- src/BaseDirs.jl
+++ src/BaseDirs.jl
@@ -36,4 +36,9 @@ include("docstrings.jl")
 
 __init__() = reload()
 
+# From testing with `--trace-compile` and `@time_imports`, these two methods
+# are solely responsible for loadtime compilation.
+precompile(Tuple{Type{NamedTuple{(), T} where T<:Tuple}, Array{String, 1}})
+precompile(Tuple{typeof(Base.get), NamedTuple{(), Tuple{}}, Symbol, String})
+
 end
