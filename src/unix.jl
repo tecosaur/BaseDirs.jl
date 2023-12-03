f7482f87dbb0149a3aa353a49055fe6e5737eed1
TEC <git@tecosaur.net>
2023-12-04 01:15:39 +0800

    Fix user-dirs.dirs handling
    I forgot about the XDG_ prefix and _DIR suffix.

--- src/unix.jl
+++ src/unix.jl
@@ -8,15 +8,16 @@ Returns a `NamedTuple` with all relevant entries.
     This is _not_ part of the BaseDirs API.
 """
 function parseuserdirs(configdir::String)
-    validnames = ("DESKTOP", "DOWNLOAD", "TEMPLATES", "PUBLICSHARE",
-                  "DOCUMENTS", "MUSIC", "PICTURES", "VIDEOS")
+    validnames = ("XDG_DESKTOP_DIR", "XDG_DOWNLOAD_DIR", "XDG_TEMPLATES_DIR",
+                  "XDG_PUBLICSHARE_DIR", "XDG_DOCUMENTS_DIR", "XDG_MUSIC_DIR",
+                  "XDG_PICTURES_DIR", "XDG_VIDEOS_DIR")
     userdirsfile = joinpath(configdir, "user-dirs.dirs")
     if isfile(userdirsfile)
         keys = Symbol[]
         values = String[]
         for line in Iterators.map(strip, eachline(userdirsfile))
             if !startswith(line, '#') && occursin('=', line)
-                key, value = split(line, '=')
+                key, value = split(line, '=', limit=2)
                 if key in validnames
                     if startswith(value, '"') && endswith(value, '"')
                         value = unescape_string(value[2:end-1])
@@ -54,14 +55,14 @@ function reload()
     # User directories
     userdirs = merge(parseuserdirs(first(CONFIG_DIRS[])),
                      parseuserdirs(CONFIG_HOME[]))
-    @setxdg DESKTOP_DIR get(userdirs, :DESKTOP, "~/Desktop")
-    @setxdg DOWNLOAD_DIR get(userdirs, :DOWNLOAD, "~/Downloads")
-    @setxdg DOCUMENTS_DIR get(userdirs, :DOCUMENTS, "~/Documents")
-    @setxdg MUSIC_DIR get(userdirs, :MUSIC, "~/Music")
-    @setxdg PICTURES_DIR get(userdirs, :PICTURES, "~/Pictures")
-    @setxdg VIDEOS_DIR get(userdirs, :VIDEOS, "~/Videos")
-    @setxdg TEMPLATES_DIR get(userdirs, :TEMPLATES, "~/Templates")
-    @setxdg PUBLICSHARE_DIR get(userdirs, :PUBLICSHARE, "~/Public")
+    @setxdg DESKTOP_DIR     get(userdirs, :XDG_DESKTOP_DIR,     "~/Desktop")
+    @setxdg DOWNLOAD_DIR    get(userdirs, :XDG_DOWNLOAD_DIR,    "~/Downloads")
+    @setxdg DOCUMENTS_DIR   get(userdirs, :XDG_DOCUMENTS_DIR,   "~/Documents")
+    @setxdg MUSIC_DIR       get(userdirs, :XDG_MUSIC_DIR,       "~/Music")
+    @setxdg PICTURES_DIR    get(userdirs, :XDG_PICTURES_DIR,    "~/Pictures")
+    @setxdg VIDEOS_DIR      get(userdirs, :XDG_VIDEOS_DIR,      "~/Videos")
+    @setxdg TEMPLATES_DIR   get(userdirs, :XDG_TEMPLATES_DIR,   "~/Templates")
+    @setxdg PUBLICSHARE_DIR get(userdirs, :XDG_PUBLICSHARE_DIR, "~/Public")
     # Other directories
     FONTS_DIRS[] =
         append!([joinpath(DATA_HOME[], "fonts"),
